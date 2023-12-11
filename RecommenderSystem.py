ab3b38a8b1c6646b99e292850dd0d2124dafaa18
lejlabrescic <lbrescic@gmail.com>
2023-12-11 13:23:01 +0100

    code changes

--- RecommenderSystem.py
+++ RecommenderSystem.py
@@ -5,25 +5,25 @@ import SentimentAnalysis as sa
 import Clustering as c
 import extractTrackGenre as etg
 
-#df = pd.read_csv("Spotify_Youtube.csv")
+df = pd.read_csv("Spotify_Youtube.csv")
 
-#df.isnull().sum()
-#data_cleaned = df.dropna()
-#df = data_cleaned
-#df.isnull().sum()
+df.isnull().sum()
+data_cleaned = df.dropna()
+df = data_cleaned
+df.isnull().sum()
 
-#df.drop(columns=["Unnamed: 0"], inplace=True)
-#df.reset_index(drop=True, inplace=True)
+df.drop(columns=["Unnamed: 0"], inplace=True)
+df.reset_index(drop=True, inplace=True)
 
-#df['song_uri'] = df['Uri'].str.split(':').str[-1]
-#df['song_lyrics'] = df.apply(lambda row: extractTrackLyrics.fetch_lyrics(row['Track'], row['Artist']), axis=1)
-#genres = etg.get_track_genre_for_df(df)
-#df['track_genre'] = genres
+df['song_uri'] = df['Uri'].str.split(':').str[-1]
+print(df)
+#df['lyrics'] = df.head(4).apply(lambda row: fetch_lyrics(row['Track'], row['Artist']), axis=1)
+#df['genres'] = df['song_uri'].apply(etg.get_track_genre_for_df)
 
 #df['polarity'], df['subjectivity'] = sa.sentiment_analysis(df, 'song_lyrics')
-#song_recommendations = c.recommend_songs(df, 'JUST DANCE HARDSTYLE', 5)
-#print("Recommended Songs:")
-#print(np.array(song_recommendations))
+song_recommendations = c.recommend_songs(df, 'JUST DANCE HARDSTYLE', 5)
+print("Recommended Songs:")
+print(np.array(song_recommendations))
 
 
 
--- extractTrackGenre.py
+++ extractTrackGenre.py
@@ -2,33 +2,21 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 import os
-
-
-def get_track_genre_for_df(df):
+def get_track_genre_for_df(song_uri):
     load_dotenv()
     client_id = os.getenv("SPOTIFY_CLIENT_ID")
     client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
     client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    genres_list = []
-    for index, row in df.iterrows():
-        track_uri = row['song_uri']
-        try:
-            track_info = sp.track(track_uri)
-            if track_info['artists']:
-                artist_id = track_info['artists'][0]['id']
-                artist_info = sp.artist(artist_id)
+    track_info = sp.track(song_uri)
+    try:
+        if track_info['artists']:
+            artist_id = track_info['artists'][0]['id']
+            artist_info = sp.artist(artist_id)
+            if artist_info['genres']:
+                print(artist_info['genres'])
+                return artist_info['genres']
+    except Exception as e:
+        print(f"Error fetching genres for track {song_uri}: {e}")
 
-                if artist_info['genres']:
-                    genres_list.append(artist_info['genres'])
-                    print(genres_list)
-                else:
-                    genres_list.append(None)
-            else:
-                genres_list.append(None)
-        except Exception as e:
-            print(f"Error fetching genres for track {track_uri}: {e}")
-            genres_list.append(None)
-
-    genres_list = df['song_uri'].apply(get_track_genre_for_df)
-    return genres_list
+    return None
