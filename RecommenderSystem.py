bac813d3fb6bad6ad25ebb0aa17bad3783396552
CausevicAmila <amila.causevic64@gmail.com>
2023-12-12 17:18:08 +0100

    small changes

--- RecommenderSystem.py
+++ RecommenderSystem.py
@@ -5,7 +5,7 @@ import SentimentAnalysis as sa
 import Clustering as c
 import extractTrackGenre as etg
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, mean_absolute_error
 import plotly.express as px
 import matplotlib.pyplot as plt
 import plotly.express as px
@@ -18,6 +18,7 @@ from sklearn.metrics import make_scorer, mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+
 
 df = pd.read_csv("Spotify_Youtube.csv")
 
@@ -38,21 +39,18 @@ df['song_uri'] = df['Uri'].str.split(':').str[-1]
 
 
 selected_columns = ['Liveness', 'Energy', 'Danceability', 'Likes', 'Stream']
+
 train_data, test_data = train_test_split(df[selected_columns], test_size=0.2, random_state=42)
 train_data, kmeans_model = c.kmeans_clustering(train_data)
 train_data, dbscan_model = c.dbscan_clustering(train_data)
+
 # sns.pairplot(train_data) 
 # plt.show()
 
 song_recommendations = c.recommend_songs(df, 'JUST DANCE HARDSTYLE', 5)
-# print("Recommended Songs:")
-# print(np.array(song_recommendations))
-# print(song_recommendations.columns)
-
-#not working for some reason 
-# ground_truth = df[df['Track'].isin(song_recommendations['Track'])]['Likes'].apply(lambda x: 1 if x > 50 else 0)
-#not working for some reason 
-# accuracy = accuracy_score(ground_truth, song_recommendations['Likes'] > 50) 
+print("Recommended Songs:")
+print(np.array(song_recommendations))
+print(song_recommendations.columns)
 
 recommended_songs_df = df[df['Track'].isin(song_recommendations['Track'])][['Track', 'Likes']]
 recommended_songs_df['Liked'] = recommended_songs_df['Likes'] > 50
@@ -68,14 +66,15 @@ accuracy = recommended_songs_df['Liked'].mean()
 
 # fig = px.bar(most_streamed1,x=most_streamed1,title="5 Most Streamed Songs")
 # fig.show()
-# most_streamed_albumtype=df.groupby(["Album_type","Track"])["Stream"].mean().sort_values(ascending=False)
+
+most_streamed_albumtype=df.groupby(["Album_type","Track"])["Stream"].mean().sort_values(ascending=False)
 # print(most_streamed_albumtype)
 # pd.concat([most_streamed_albumtype.unstack(0).nlargest(5,['album']) ,most_streamed_albumtype.unstack(0).nlargest(5,['single'])]).plot.bar()
 # plt.show()
 
-# most_played_artist_spotify=df.groupby("Artist")["Stream"].mean().sort_values(ascending=False)
+most_played_artist_spotify=df.groupby("Artist")["Stream"].mean().sort_values(ascending=False)
 # print(most_played_artist_spotify)
-# most_played_artist_spotify1=most_played_artist_spotify.nlargest(5)
+most_played_artist_spotify1=most_played_artist_spotify.nlargest(5)
 
 # fig1=px.bar(most_played_artist_spotify1,title="Most played artist on spotify")
 # fig1.show()
@@ -85,25 +84,6 @@ accuracy = recommended_songs_df['Liked'].mean()
 
 # print(f.nlargest(10,["Stream","Views"]))
 
-#cluser evaluation metrics 
-# silhouette_avg = silhouette_score(train_data[selected_columns], train_data['KMeans_Cluster'])
-# print(f"Silhouette Score: {silhouette_avg}")  #prema rezultatima okej su definisani
-# print(f"KMeans Inertia: {kmeans_model.inertia_}") #prema outputu nama je velik broj sto je manji to znaci da su tacke blize cluseru, znaci da su nama daleko
-
-#recommender evaluatin ne valja nam bas clusering ili cu nam daleko ili su oberfitted
-# ground_truth = recommended_songs_df['Liked']
-# predicted_labels = recommended_songs_df['Likes'] > 50
-
-# precision = precision_score(ground_truth, predicted_labels)
-# recall = recall_score(ground_truth, predicted_labels)
-# f1 = f1_score(ground_truth, predicted_labels)
-
-# print(f"Precision: {precision}") # rasult: all the instances predicted as positive (Liked) were indeed positive.
-# print(f"Recall: {recall}") #result:  model correctly identified all positive instances in the dataset.
-# print(f"F1-Score: {f1}") #a perfect balance between precision and recall
-
-#ovi rezultati znace da nam je data set ili nije u balansu pa lase dobije ove pozitivne vjv njih ima vise
-#a mozda sam ja samo glupa pa sam koristila isto u treniranju i testiranju :)
 
 # Visualizing recommender results
 # fig = px.bar(recommended_songs_df, x='Track', y='Likes', color='Liked', title='Recommender Results')
@@ -118,4 +98,6 @@ regression_model = LinearRegression()
 regression_model.fit(X_train, y_train)
 y_pred = regression_model.predict(X_test)
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+mae = mean_absolute_error(y_test, y_pred)
+print("Mean Absolute Error (MAE):", mae)
 print("Root Mean Squared Error (RMSE):", rmse)
